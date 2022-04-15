00fd13e475f0d37a8c59bdae88433966f099164e
ParkGyueun <22100271@handong.ac.kr>
2022-04-15 19:23:42 +0900

    C2

--- test.c
+++ test.c
@@ -1,7 +1,7 @@
 int main(){
-	int num;
-	printf("input number: ");
-	scanf("%d", &num);
-	printf("input number is %d\n", num);
+	int num1, num2;
+	printf("input numbers: ");
+	scanf("%d %d", &num1, &num2);
+	printf("input numbers are %d and %d\n", num1, num2);
 	return 0;
 }
