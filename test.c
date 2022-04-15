8a41fa20eb1627fbdfcf04720c000423c2007cda
ParkGyueun <22100271@handong.ac.kr>
2022-04-15 19:26:25 +0900

    C3

--- test.c
+++ test.c
@@ -1,7 +1,10 @@
 int main(){
 	int num1, num2;
+	int sum=0;
 	printf("input numbers: ");
 	scanf("%d %d", &num1, &num2);
+	sum = num1 + num2;
 	printf("input numbers are %d and %d\n", num1, num2);
+	printf("sum is %d\n", sum);
 	return 0;
 }
