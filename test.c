1c3b7b2eb5c02aa8339e996fc1c6cfe0cb905523
ParkGyueun <22100271@handong.ac.kr>
2022-04-15 19:35:34 +0900

    C6

--- test.c
+++ test.c
@@ -2,12 +2,15 @@ int main(){
 	int num1, num2;
 	int sum = 0;
 	int sub = 0;
+	int mul = 0;
 	printf("input numbers: ");
 	scanf("%d %d", &num1, &num2);
 	sum = num1 + num2;
-	sub = num1 - num2:
+	sub = num1 - num2;
+	mul = num1 * num2;
 	printf("input numbers are %d and %d\n", num1, num2);
 	printf("sum is %d\n", sum);
 	printf("sub is %d\n", sub);
+	printf("mul is %d\n", mul);
 	return 0;
 }
